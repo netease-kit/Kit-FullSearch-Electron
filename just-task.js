a50ba8ce02077463d1117dd08ca080c42356928a
Dylan <2894220@gmail.com>
2021-07-14 16:51:34 +0800

    just-scripts: add target_platform&target_arch argument for just install
    Signed-off-by: Dylan <2894220@gmail.com>

--- just-task.js
+++ just-task.js
@@ -3,15 +3,20 @@ const fs = require('fs')
 const download = require('download')
 const path = require('path')
 const downloadUrl = 'http://yx-web.nos.netease.com/package/1626234782/electron-tokenizer-plugin_v1.1.0.tar.gz'
+
 task('install', () => {
   return new Promise((resolve, reject) => {
     const localPath = path.join(__dirname, 'lib')
     download(downloadUrl, localPath, {
       extract: true
     }).then(() => {
+      const platform = process.env.npm_config_target_platform || process.platform
+      const arch = process.env.npm_config_target_arch || process.arch
       logger.info(`[install] Download prebuilt binaries from ${downloadUrl}`)
-      if (process.platform === 'win32') {
-        const src = path.join(localPath, process.arch, 'simple.dll')
+      logger.info(`[install] Target platform: ${platform}`)
+      logger.info(`[install] Target arch: ${arch}`)
+      if (platform === 'win32') {
+        const src = path.join(localPath, arch, 'simple.dll')
         const dst = path.join(localPath, 'simple.dll')
         logger.info(`Copy win32 lib from ${src} to ${dst}`)
         fs.copyFileSync(src, dst)
