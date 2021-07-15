fda0d8252a0a72abcc8bda28b6dcc8350bdc7ba1
Dylan <2894220@gmail.com>
2021-07-15 13:33:12 +0800

    modified download URL of 3rd libraries
    Signed-off-by: Dylan <2894220@gmail.com>

--- just-task.js
+++ just-task.js
@@ -2,11 +2,12 @@ const { task, option, logger, argv } = require('just-scripts')
 const fs = require('fs')
 const download = require('download')
 const path = require('path')
-const downloadUrl = 'http://yx-web.nos.netease.com/package/electron-tokenizer-plugin.tar.gz'
 
 task('install', () => {
   return new Promise((resolve, reject) => {
+    const curPkgMeta = require(path.join(__dirname, 'package.json'))
     const localPath = path.join(__dirname, 'lib')
+    const downloadUrl = `http://yx-web.nos.netease.com/package/electron-tokenizer-plugin_v${curPkgMeta.version}.tar.gz`
     download(downloadUrl, localPath, {
       extract: true
     }).then(() => {
